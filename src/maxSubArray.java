e823697c9188c7fdeb5776371ae0625c25e1e09a
ankit <ankitkejriwaal@gmail.com>
2020-01-19 20:13:37 -0500

    added logic with O(n2) complexity

--- src/maxSubArray.java
+++ src/maxSubArray.java
@@ -2,6 +2,7 @@ public class maxSubArray {
 
 
     public static  int bruteForce(int[] arr){
+//        Time complexity is O(n**3)
         int ans =0;
         for(int i=1;i<arr.length;i++){
             for(int start_index =0;start_index<arr.length;start_index++){
@@ -17,9 +18,28 @@ public class maxSubArray {
         }
         return ans;
     }
+
+    public static  int bruteForce1(int[] arr){
+//        Time complexity is O(n**2)
+        int ans =0;
+        for(int start_index=0;start_index<arr.length;start_index++){
+            int sum=0;
+            for(int j=1;j<=arr.length;j++){
+                if((start_index+j)>= arr.length){
+                    break;
+                }
+                sum += arr[start_index+j-1];
+                ans = Math.max(sum,ans);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args){
-        int[] arr = {2,-5,4,6,4,-5,6,-14,12,-8};
+        int[] arr = {2,-5,4,6,4,-5,6,14,12,-8};
         int res = bruteForce(arr);
         System.out.println("Maximum sum: " +res);
+        int res1 = bruteForce1(arr);
+        System.out.println("Maximum sum: " +res1);
     }
 }
